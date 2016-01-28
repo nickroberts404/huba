003a5fe1032b967d01fc1667f6eeea74db0a03d2
nickroberts404 <nickroberts404@gmail.com>
2016-01-28 02:22:37 -0600

    Added colors with chalk

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 #!/usr/bin/env node
 
-var fs = require('fs');
-var open = require('open');
+const fs = require('fs');
+const open = require('open');
+const chalk = require('chalk');
 
-var thepackage = process.env.PWD + '/package.json'
+const thepackage = process.env.PWD + '/package.json'
 fs.readFile(thepackage, 'utf-8', (err, data) => {
 	// File can't be found
-	if (err) return console.error('Sorry, package.json can\'t be found...');
+	if (err) return error('Sorry, package.json can\'t be found...');
 	// File has been found
 	var p = JSON.parse(data);
 	if (p.repository && p.repository.url && (p.repository.type == 'git' || !p.repository.type)) {
 		// Withdraw link from git repo string
 		var link = processLink(p.repository.url);
-		console.log('Opening ' + link);
+		console.log(chalk.green('Opening ' + link));
 		open(link , a => {
 			// Link is broken
-			if (a) return console.error('Sorry, theres been an error opening the link...');
+			if (a) return error('Sorry, theres been an error opening the link...');
 		});
 	} else {
-		return console.error('Sorry, Github link can\'t be found...');
+		return error('Sorry, Github link can\'t be found...');
 	}
 })
 // Finds the appropriate link. Typically removes the +git from the beginning and .git from the end.
@@ -26,4 +27,8 @@ function processLink(link) {
 	var start = link.indexOf('github.com');
 	var end = link.indexOf('.git');
 	return 'https://' + link.slice(start, end);
+}
+// Logs an error
+function error(err) {
+	return console.error(chalk.red(err));
 }
